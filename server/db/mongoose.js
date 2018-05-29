65d40c1ab47712cc48da7d545dd92ce523b91ba3
mimoncopitman <mimoncopitman@gmail.com>
2018-05-29 14:54:41 +0300

    - Changed some variables for production environment.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //this is for production environment MONGODB_URI is only on heroku
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
